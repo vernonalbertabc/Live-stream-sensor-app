a26a33fb0ebd6156fed73fcfce9200b063235862
vernon.dsouza123 <vernon.dsouza123@gmail.com>
2021-07-23 19:11:59 +0200

    Check if app is working

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask import Flask, jsonify,request,render_template,make_response
 import numpy as np
 import time
 import json
-import random
 
 app = Flask(__name__)
 
@@ -17,7 +16,7 @@ def main():
 
 @app.route("/data",methods=["GET","POST"])
 def get():
-    data = [time.time()*100,np.sin(time.time())*100 + random.random()*100]
+    data = [time.time()*100,np.sin(time.time())*100 + np.random.random_sample()*100]
     response = make_response(json.dumps(data))
     response.content_type = "application/json"
 
