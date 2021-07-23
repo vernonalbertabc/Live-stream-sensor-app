1d2186889d93689d124ad6e9fac1125a3b17e3d6
vernon.dsouza123 <vernon.dsouza123@gmail.com>
2021-07-23 17:59:13 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Cloud deplyoment of sensor app
+
+```  
+1. Simple example of deployment of a livestream sensor readout app. 
+2. App created with Flask, JavaScript(within HTML) and HTML.
+3. Deployment in Heroku
+
+```
---
+++ app.py
@@ -0,0 +1,29 @@
+# Reference
+#https://www.youtube.com/watch?v=e68S9x3Rh_0&ab_channel=soumilshah1995
+
+from flask import Flask, jsonify,request,render_template,make_response
+import random
+import numpy as np
+import time
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    
+    return render_template("index.html")
+
+
+@app.route("/data",methods=["GET","POST"])
+def get():
+    data = [time.time()*100,np.sin(time.time())*100 + random.random()*100]
+    response = make_response(json.dumps(data))
+    response.content_type = "application/json"
+
+    return response
+
+if __name__ == "__main__":
+    app.run()
+
+
